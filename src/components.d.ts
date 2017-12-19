a0e7aac0c573210571054e6f715e37905b20f562
jgw96 <jgw9617@gmail.com>
2017-12-19 14:03:51 -0600

    fix(package): update for latest stencil/core

--- src/components.d.ts
+++ src/components.d.ts
@@ -4,34 +4,34 @@
  * and imports for stencil collections that might be configured in your stencil.config.js file
  */
 
-import { MyComponent as MyComponent } from './components/my-component/my-component';
 
-interface HTMLMyComponentElement extends MyComponent, HTMLElement {
-}
-declare var HTMLMyComponentElement: {
-  prototype: HTMLMyComponentElement;
-  new (): HTMLMyComponentElement;
-};
+import {
+  MyComponent as MyComponent
+} from './components/my-component/my-component';
+
 declare global {
+  interface HTMLMyComponentElement extends MyComponent, HTMLElement {
+  }
+  var HTMLMyComponentElement: {
+    prototype: HTMLMyComponentElement;
+    new (): HTMLMyComponentElement;
+  };
   interface HTMLElementTagNameMap {
-      "my-component": HTMLMyComponentElement;
+    "my-component": HTMLMyComponentElement;
   }
   interface ElementTagNameMap {
-      "my-component": HTMLMyComponentElement;
+    "my-component": HTMLMyComponentElement;
   }
   namespace JSX {
-      interface IntrinsicElements {
-          "my-component": JSXElements.MyComponentAttributes;
-      }
+    interface IntrinsicElements {
+      "my-component": JSXElements.MyComponentAttributes;
+    }
   }
   namespace JSXElements {
-      export interface MyComponentAttributes extends HTMLAttributes {
-          mode?: string,
-          color?: string,
-        
-          first?: string,
-          last?: string
-      }
+    export interface MyComponentAttributes extends HTMLAttributes {
+      first?: string;
+      last?: string;
+    }
   }
 }
 
