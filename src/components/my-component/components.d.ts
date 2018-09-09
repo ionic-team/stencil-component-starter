fcbdb55d015d96d8cb7eacfcd9b741fee15d2911
tallyb <tally.barak@gmail.com>
2018-09-09 12:01:09 +0300

    Update packages and bit compiler

--- src/components/my-component/components.d.ts
+++ src/components/my-component/components.d.ts
@@ -61,3 +61,5 @@ declare global {
 }
 
 declare global { namespace JSX { interface StencilJSX {} } }
+
+export declare function defineCustomElements(window: any): void;
--- stencil.config.js
+++ stencil.config.js
@@ -1,5 +1,5 @@
 exports.config = {
-  namespace: 'mycomponent',
+  namespace: 'stencomp',
   outputTargets:[
     {
       type: 'dist'
