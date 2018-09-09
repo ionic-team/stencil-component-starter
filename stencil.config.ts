926c6a9531d4356eb383b30cf7e390c88b5e241d
tallyb <tally.barak@gmail.com>
2018-09-09 12:09:07 +0300

    merge ionic

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD:stencil.config.js
-exports.config = {
-  namespace: 'stencomp',
-=======
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'mycomponent',
->>>>>>> 69a1829c93a44b1b324d8a5646032cafcdd40306:stencil.config.ts
+  namespace: 'stencomp',
   outputTargets:[
     {
       type: 'dist'
