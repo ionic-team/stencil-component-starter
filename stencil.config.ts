09c280500653e77dc3d0111bc9cd26f2b48ea516
peterpeterparker <david.dalbusco@outlook.com>
2021-12-06 17:13:52 +0100

    build: drop dist-custom-elements-bundle for dist-custom-elements

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,7 +8,7 @@ export const config: Config = {
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements-bundle',
+      type: 'dist-custom-elements',
     },
     {
       type: 'docs-readme',
