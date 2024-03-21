83f71169a7b55d414971245f36adbc054a3a1b12
Christian Bromann <git@bromann.dev>
2024-03-21 16:28:31 -0700

    chore(starter-project): Avoid confusions on distribution approaches (#134)
    * chore(starter-project): Avoid confusions on distribution approaches
    
    * remove es2015 and es2017 exports
    
    * document purpose of index.ts
    
    * update docs on distributing components
    
    * set customElementsExportBehavior
    
    * set externalRuntime to false - fixes #95
    
    * update links
    
    * Update readme.md
    
    Co-authored-by: Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
    
    * PR comments
    
    ---------
    
    Co-authored-by: Ryan Waskiewicz <ryanwaskiewicz@gmail.com>

--- stencil.config.ts
+++ stencil.config.ts
@@ -9,6 +9,8 @@ export const config: Config = {
     },
     {
       type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
     },
     {
       type: 'docs-readme',
